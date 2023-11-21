1c8a1a9772176abd3bb90092274d6e56d4a072b2
Will <willycosh@gmail.com>
2023-11-21 06:00:19 +0000

    More work on AABB

--- src/main/java/com/dsd/tbb/customs/models/ModelUtils.java
+++ src/main/java/com/dsd/tbb/customs/models/ModelUtils.java
@@ -2,16 +2,13 @@ package com.dsd.tbb.customs.models;
 
 import com.dsd.tbb.util.TBBLogger;
 import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.VertexConsumer;
 import net.minecraft.world.phys.AABB;
 import org.joml.Matrix4f;
-import org.joml.Vector3d;
 import org.joml.Vector3f;
 import org.joml.Vector4f;
-import software.bernie.geckolib.cache.object.GeoBone;
-import software.bernie.geckolib.cache.object.GeoCube;
 import software.bernie.geckolib.cache.object.GeoQuad;
 import software.bernie.geckolib.cache.object.GeoVertex;
-import software.bernie.geckolib.util.RenderUtils;
 
 import java.text.NumberFormat;
 import java.util.ArrayList;
@@ -19,48 +16,23 @@ import java.util.ArrayList;
 public class ModelUtils {
 
 
-    public static void transformPoseStackForBone(PoseStack bonePoseStack, GeoBone bone){
-        RenderUtils.translateMatrixToBone(bonePoseStack,bone);
-        RenderUtils.translateToPivotPoint(bonePoseStack,bone);
-        RenderUtils.rotateMatrixAroundBone(bonePoseStack,bone);
-        RenderUtils.scaleMatrixForBone(bonePoseStack,bone);
-        bonePoseStack.popPose();
-    }
-
-    public static Vector3f[] transformCubeVertices(PoseStack poseStack, GeoBone bone) {
+    public static Vector3f[] createAndCaptureVerticesOfQuad(GeoQuad quad, Matrix4f poseState, Vector3f normal, VertexConsumer buffer,
+                                        int packedLight, int packedOverlay, float red, float green, float blue, float alpha) {
+        TBBLogger.getInstance().debug("createAndCapture","Called");
         ArrayList<Vector3f> transformedVertices = new ArrayList<>();
-
-        Matrix4f poseState = new Matrix4f(poseStack.last().pose());
-        for (GeoCube cube : bone.getCubes()) {
-            for(GeoQuad quad : cube.quads()) {
-                for (GeoVertex vertex : quad.vertices()) {
-                    Vector3f position = vertex.position();
-                    Vector4f vector4f = poseState.transform(new Vector4f(position.x(), position.y(), position.z(), 1.0f));
-                    transformedVertices.add(new Vector3f(vector4f.x(), vector4f.y(), vector4f.z()));
-                }
-            }
+        for (GeoVertex vertex : quad.vertices()) {
+            Vector3f position = vertex.position();
+            Vector4f vector4f = poseState.transform(new Vector4f(position.x(), position.y(), position.z(), 1.0f));
+            transformedVertices.add(new Vector3f(vector4f.x(), vector4f.y(), vector4f.z()));
+            buffer.vertex(vector4f.x(), vector4f.y(), vector4f.z(), red, green, blue, alpha, vertex.texU(),
+                    vertex.texV(), packedOverlay, packedLight, normal.x(), normal.y(), normal.z());
         }
+
         return transformedVertices.toArray(new Vector3f[0]);
     }
 
 
-    public static PoseStack copyPoseStack(PoseStack original) {
-        PoseStack newStack = new PoseStack();
-
-        // This assumes you're interested in copying the current (top) transformation state
-        if (!original.clear()) {
-            // Get the current top matrix from the original stack
-            Matrix4f currentMatrix = original.last().pose();
-
-            // Apply a copy of this matrix to the new stack
-            newStack.pushPose();
-            newStack.last().pose().set(currentMatrix);
-        }
-
-        return newStack;
-    }
-
-    public static AABB createAABBFromVertices(Vector3d[] vertices) {
+    public static AABB createAABBFromVertices(Vector3f[] vertices) {
         if (vertices == null || vertices.length == 0) {
             // Handle the empty or null array case
             // For example, return a default AABB or throw an exception
@@ -74,7 +46,7 @@ public class ModelUtils {
         double maxY = Double.MIN_VALUE;
         double maxZ = Double.MIN_VALUE;
 
-        for (Vector3d vertex : vertices) {
+        for (Vector3f vertex : vertices) {
             minX = Math.min(minX, vertex.x());
             minY = Math.min(minY, vertex.y());
             minZ = Math.min(minZ, vertex.z());
@@ -88,67 +60,40 @@ public class ModelUtils {
 
 
 
-    public static void outputQuadVertices(GeoBone bone, Vector3f[] quadVertices){
+    public static void outputQuadVertices(Vector3f[] quadVertices){
         StringBuilder sa = new StringBuilder();
         NumberFormat nf = NumberFormat.getNumberInstance();
         nf.setMaximumFractionDigits(2);
         nf.setMinimumFractionDigits(2);
-        sa.append("\nBone ").append(bone.getName()).append(" Quad Vertices:\n");
+        sa.append("\n******Quad Vertices********\n");
 
         for(int j=0; j< quadVertices.length; j++){
             sa.append("\t[").append(j).append("] ").append(quadVertices[j].x).append(",");
             sa.append(quadVertices[j].y).append(",").append(quadVertices[j].z).append("\n");
         }
+        sa.append("----------------------------\n");
+
         TBBLogger.getInstance().debug("BONE INFO",sa.toString());
     }
+    public static void outputPoseStackState(PoseStack poseStack) {
+        if (!poseStack.clear()) {
+            Matrix4f currentMatrix = new Matrix4f(poseStack.last().pose());
+            // Convert the matrix to a readable string format
+            String matrixString = matrixToString(currentMatrix);
+            // Output the matrix (you can replace this with your logging mechanism)
+            TBBLogger.getInstance().debug ("Output Pose Stack","Current PoseStack State: \n" + matrixString);
+        } else {
+            TBBLogger.getInstance().debug ("Output Pose Stack","PoseStack is empty.");
+        }
+    }
 
-    /*public static void outputBoneInfo(PoseStack poseStack, GeoBone bone){
-        StringBuilder sa = new StringBuilder();
+    private static String matrixToString(Matrix4f matrix) {
         NumberFormat nf = NumberFormat.getNumberInstance();
         nf.setMaximumFractionDigits(2);
         nf.setMinimumFractionDigits(2);
-        sa.append("Bone ").append(bone.getName()).append(" |\n");
-        sa.append("World Transform\n").append(bone.getWorldSpaceMatrix().toString(nf)).append("\n");
-        sa.append("Local Space Matrix\n").append(bone.getLocalSpaceMatrix().toString(nf)).append("\n");
-        sa.append("World Space Norm Matrix\n").append(bone.getWorldSpaceNormal().toString(nf)).append("\n");
-
-        sa.append("Position Vector = ").append(bone.getPositionVector().toString(nf)).append("\n");
-        sa.append("Rotation Vector = ").append(bone.getRotationVector().toString(nf)).append("\n");
-        sa.append("Scale Vector = ").append(bone.getScaleVector().toString(nf)).append("\n");
-
-        sa.append("Local Position = ").append(bone.getLocalPosition().toString(nf)).append("\n");
-        sa.append("Relative to World Position =").append(bone.getWorldPosition().toString(nf)).append("\n");
-        sa.append("Relative to Model Position =").append(bone.getModelPosition().toString(nf)).append("\n");
-
-        sa.append("Cube Information\n");
-        int i = 0;
-        for(GeoCube cube : bone.getCubes()){
-            Vector3f[] tQuadVertices = transformCubeVertices(poseStack,cube);
-            sa.append("\t[").append(i).append("] Inflate = ").append(cube.inflate()).append("\n");
-            sa.append("\t[").append(i).append("] Size = ").append(cube.size().toString()).append("\n");
-            sa.append("\t[").append(i).append("] Pivots = ").append(cube.pivot().toString()).append("\n");
-            sa.append("\tGeo Quad Vertices\n");
-            int j = 0;
-            for(GeoQuad quad : cube.quads()){
-                int k = 0;
-                sa.append("\t\t[").append(j).append("] Direction - ").append(quad.direction().toString()).append("\n");
-                for(GeoVertex vertex : quad.vertices()){
-                    sa.append("\t\t\t[").append(k).append("] ").append(vertex.position().toString(nf)).append("\n");
-                    k++;
-                }
-                j++;
-            }
-            sa.append("\tQUAD Transformed Vertices\n");
-            for(j=0; j< tQuadVertices.length; j++){
-                sa.append("\t\t\t[").append(j).append("] ").append(tQuadVertices[j].x).append(",");
-                sa.append(tQuadVertices[j].y).append(",").append(tQuadVertices[j].z).append("\n");
-            }
-            i++;
-        }
-
-
-        TBBLogger.getInstance().debug("BONE INFO",sa.toString());
-
-    }*/
+        // Create a string representation of the matrix
+        // Format the string as needed for readability
+        return matrix.toString(nf);
+    }
 
 }
--- src/main/java/com/dsd/tbb/customs/renderers/EndGiantRenderer.java
+++ src/main/java/com/dsd/tbb/customs/renderers/EndGiantRenderer.java
@@ -5,26 +5,32 @@ import com.dsd.tbb.customs.entities.endgiant.EndGiantPart;
 import com.dsd.tbb.customs.models.EndGiantModel;
 import com.dsd.tbb.customs.models.ModelUtils;
 import com.dsd.tbb.main.TrialsByBaby;
+import com.dsd.tbb.util.TBBLogger;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
 import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.debug.DebugRenderer;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.phys.AABB;
-import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
+import org.joml.Matrix4f;
 import org.joml.Vector3f;
 import software.bernie.geckolib.cache.object.BakedGeoModel;
 import software.bernie.geckolib.cache.object.GeoBone;
+import software.bernie.geckolib.cache.object.GeoCube;
+import software.bernie.geckolib.cache.object.GeoQuad;
 import software.bernie.geckolib.renderer.GeoEntityRenderer;
+import software.bernie.geckolib.util.RenderUtils;
 
-import javax.annotation.Nullable;
+import java.util.Objects;
 
 @OnlyIn(Dist.CLIENT)
 public class EndGiantRenderer extends GeoEntityRenderer<EndGiant>{
-
+    private String currentBoneRendering;
+    private EndGiant curGiantRendering;
     private static final ResourceLocation TEXTURE = new ResourceLocation(TrialsByBaby.MOD_ID, "textures/endgiant.png");
 
     public EndGiantRenderer(EntityRendererProvider.Context context) {
@@ -37,55 +43,96 @@ public class EndGiantRenderer extends GeoEntityRenderer<EndGiant>{
     }
 
     @Override
-    public void preRender(PoseStack poseStack, EndGiant animatable, BakedGeoModel model, @Nullable MultiBufferSource bufferSource, @Nullable VertexConsumer buffer, boolean isReRender,
-                           float partialTick, int packedLight, int packedOverlay, float red, float green, float blue, float alpha) {
-        poseStack.pushPose();
-
-        for (EndGiantPart part : animatable.getEGPParts()) {
-            GeoBone bone = model.getBone(part.getPartName()).orElse(null);
-            if (bone != null) {
-                // Create a new copy of the pose stack for each bone
-                PoseStack bonePoseStack = ModelUtils.copyPoseStack(poseStack);
-
-                // Apply transformations specific to this bone
-                ModelUtils.transformPoseStackForBone(bonePoseStack, bone);
-
-                // Transform vertices and output them
-                Vector3f[] transformedVertices = ModelUtils.transformCubeVertices(bonePoseStack, bone);
-                ModelUtils.outputQuadVertices(bone, transformedVertices);
-            }
-        }
-
-        poseStack.popPose();  // Ensure to pop the pose stack after processing all bones
-
-        super.preRender(poseStack, animatable, model, bufferSource, buffer, isReRender, partialTick, packedLight, packedOverlay, red, green, blue, alpha);
+    public void preRender(PoseStack poseStack, EndGiant animatable, BakedGeoModel model, MultiBufferSource bufferSource,
+                          VertexConsumer buffer, boolean isReRender, float partialTick, int packedLight, int packedOverlay,
+                          float red, float green, float blue,float alpha) {
+        TBBLogger.getInstance().debug("*********preRender","Called");
+        super.preRender(poseStack,animatable,model,bufferSource,buffer,isReRender,partialTick,packedLight,packedOverlay,
+                red,green,blue,alpha);
 
     }
+    @Override
+    public void render(EndGiant entity, float entityYaw, float partialTick, PoseStack poseStack, MultiBufferSource bufferSource, int packedLight){
+        TBBLogger.getInstance().debug("---------render","Called");
+        super.render(entity,entityYaw,partialTick,poseStack,bufferSource,packedLight);
+    }
+
+    @Override
+    public void actuallyRender(PoseStack poseStack, EndGiant animatable, BakedGeoModel model, RenderType renderType,
+                               MultiBufferSource bufferSource, VertexConsumer buffer, boolean isReRender, float partialTick,
+                               int packedLight, int packedOverlay, float red, float green, float blue, float alpha) {
+        TBBLogger.getInstance().debug("actuallyRender","Called");
+            super.actuallyRender(poseStack,animatable,model,renderType,bufferSource,buffer,isReRender,partialTick,packedLight,
+                                packedOverlay,red,green,blue,alpha);
+    }
+
+    @Override
+    public void renderRecursively(PoseStack poseStack, EndGiant animatable, GeoBone bone, RenderType renderType, MultiBufferSource bufferSource,
+                                  VertexConsumer buffer, boolean isReRender, float partialTick, int packedLight,
+                                  int packedOverlay, float red, float green, float blue, float alpha) {
+        TBBLogger.getInstance().debug("renderRecursively","Bone Active = " + bone.getName());
+        this.curGiantRendering = animatable;
+        super.renderRecursively(poseStack,animatable,bone,renderType,bufferSource,buffer,isReRender,partialTick,packedLight,
+                                packedOverlay,red,green,blue,alpha);
+    }
+
+    @Override
+    public void renderCubesOfBone(PoseStack poseStack, GeoBone bone, VertexConsumer buffer, int packedLight,
+                                  int packedOverlay, float red, float green, float blue, float alpha) {
+        TBBLogger.getInstance().debug("renderCubeOfBones","Bone Active = " + bone.getName());
+        this.currentBoneRendering = bone.getName();
+        super.renderCubesOfBone(poseStack,bone,buffer,packedLight,packedOverlay,red,green,blue,alpha);
+    }
+
+    @Override
+    public void renderChildBones(PoseStack poseStack, EndGiant animatable, GeoBone bone, RenderType renderType, MultiBufferSource bufferSource,
+                                 VertexConsumer buffer,boolean isReRender, float partialTick, int packedLight, int packedOverlay,
+                                 float red, float green, float blue, float alpha) {
+        TBBLogger.getInstance().debug("renderChildBones","Bone Active = " + bone.getName());
+        super.renderChildBones(poseStack,animatable,bone,renderType,bufferSource,buffer,isReRender,partialTick,packedLight,
+                packedOverlay,red,green,blue,alpha);
+    }
+    @Override
+    public void renderCube(PoseStack poseStack, GeoCube cube, VertexConsumer buffer, int packedLight,
+                    int packedOverlay, float red, float green, float blue, float alpha) {
+        TBBLogger.getInstance().debug("renderCube","Called");
+        // Apply transformations to the cube
+        RenderUtils.translateToPivotPoint(poseStack, cube);
+        RenderUtils.rotateMatrixAroundCube(poseStack, cube);
+        RenderUtils.translateAwayFromPivotPoint(poseStack, cube);
+        // Get the current transformation matrix
+        Matrix4f poseState = poseStack.last().pose();
+
+        for (GeoQuad quad : cube.quads()) {
+            if (quad == null) continue;
+
+            Vector3f normal = poseStack.last().normal().transform(new Vector3f(quad.normal()));
+            RenderUtils.fixInvertedFlatCube(cube, normal);
+
+            // Create vertices and capture their transformed positions
+            Vector3f[] quadVerts = ModelUtils.createAndCaptureVerticesOfQuad(quad, poseState, normal, buffer, packedLight, packedOverlay, red, green, blue, alpha);
+            AABB boundingBox = ModelUtils.createAABBFromVertices(quadVerts);
+            for(EndGiantPart part :  this.curGiantRendering.getEGPParts()){
+                if(Objects.equals(part.getPartName(), this.currentBoneRendering)){
+                    part.setBoundingBox(boundingBox);
+                }
+            }
+
+        }
+    }
+
 
     public void setPartBB(EndGiant entity) {
 
     }
 
-    private AABB calculateBoundingBox(GeoBone bone) {
 
-
-        return new AABB(0,0,0,0,0,0);
-    }
-
-    private Vec3 applyTransformations(Vec3 point, Vec3 position, Vec3 rotation) {
-
-        return new Vec3(0,0,0);
-    }
 
 
 
 
     private void drawBoundingBox(PoseStack poseStack, MultiBufferSource bufferSource, EndGiantPart part, float partialTicks) {
-        // Get the part's position relative to the entity
-        Vec3 partPosition = part.position();
 
-        poseStack.pushPose();
-        poseStack.translate(partPosition.x(), partPosition.y(), partPosition.z());
 
         DebugRenderer.renderFilledBox(poseStack, bufferSource, part.getBoundingBox(), 1.0F, 1.0F, 1.0F, 1.0F);
 
